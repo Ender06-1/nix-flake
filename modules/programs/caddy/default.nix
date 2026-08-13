0788b6e4a376948a752c29e4146d6f03450ba4ec
Admin <ndxendernight@gmail.com>
2026-08-13 03:27:25 +0200

    update: fujitsu

--- modules/programs/caddy/default.nix
+++ modules/programs/caddy/default.nix
@@ -11,7 +11,7 @@
 
         package = pkgs.caddy.withPlugins {
           plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20260106222316-bb080c4414ac" ];
-          hash = "sha256-XBdYjtuPVu/beIgFgFcVp6ln4r9kq0B6+4xJ8+WWYn0=";
+          hash = "sha256-tP/ZQjZvfb+e3322dzd3I89Y9QwujcyqV1fbNWyw08g=";
         };
         environmentFile = config.age.secrets.caddy.path;
       };
