0885d8e262fe986dfa72dfde9c7212e78af55c7f
Mathéba <ndxendernight@gmail.com>
2026-09-20 10:36:09 +0200

    update

--- modules/programs/neovim/config/init.lua
+++ modules/programs/neovim/config/init.lua
@@ -5,10 +5,7 @@ require("config.lazy")
 vim.cmd("colorscheme onedark")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_yank", {}),
-  desc = "Highlight selection on yank",
-  pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 500 })
+    vim.highlight.on_yank({ timeout = 200 })
   end,
 })
